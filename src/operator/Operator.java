a663ac9dc16fcb4fa6baf95f465b04d7bbf689a6
Kimgyuilli <87853959+Kimgyuilli@users.noreply.github.com>
2024-07-04 01:32:12 +0900

    feat - java start - 연산자

---
+++ src/operator/Operator.java
@@ -0,0 +1,28 @@
+package operator;
+
+public class Operator {
+    public static void main(String[] args) {
+        //변수 초기화
+        int a = 5;
+        int b = 2;
+
+        //덧셈
+        int sum = a+b;
+        System.out.println("sum = " + sum);
+
+        int diff = a-b ;
+        System.out.println("diff = " + diff);
+
+        //곱셈
+        int multi = a*b;
+        System.out.println("multi = " + multi);
+
+        //나눗셈
+        int div = a/b;
+        System.out.println("div = " + div);
+
+        //나머지
+        int mod = a%b;
+        System.out.println("mod = " + mod);
+    }
+}
